0b0778cd491678c92282220c8338318b8556aeba
yunusemre <yunus.emredemir222002@gmail.com>
2020-05-31 17:04:14 +0300

    Reviews Language detection.

---
+++ Teknolojiler/languageDetect.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from langdetect import detect
+#print(detect('今一はお前さん'))
+
+reviews_df = pd.read_csv("C:/Users/Demir/Desktop/Final_Project/DataSets/London_new1.csv", encoding = "ISO-8859-1")   # read data   !!!!!
+reviews_df = reviews_df[reviews_df['Review Text'].str.contains("<U") == False] # Remove some reviews created full of unknown characters.
+reviews_df_com = reviews_df[['Review Text','Review Rating', 'Property Name']]
+reviews_df_com = reviews_df_com.dropna().copy() # Remove emty review.
+#reviews_df_com = reviews_df_com.sample(frac = 0.01, replace = False, random_state=42) # Toplamda 15000 yarom var bunların %1 ini yani 1500 tanesini işleme koy.
+print(reviews_df_com.describe())
+
+j = 0
+for t in range(len(reviews_df_com)):  # iterate for each object
+    i = str(reviews_df_com['Review Text'].values[t])  # Take just reviews to String : i
+    #print(i)
+    if detect(i) != "en":
+        print(i)
+        #reviews_df_com = reviews_df_com.drop(index=reviews_df_com['Review Text'].index[t])
+        j +=1
+
+print("toplam: {},  English: {}, Others: {}".format(len(reviews_df_com), j, len(reviews_df_com)-j))
+
+
