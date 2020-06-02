c69b6577b4e15d01ff4fe89ba1ddd30260349d17
yunusemre <yunus.emredemir222002@gmail.com>
2020-06-03 02:15:38 +0300

    avgRaiting + sifatlar1 + coloredprint + colorful reviews +
    sifatlar1.csv added.(took from vader)
    coloredprint: class bcolors: added
    real avg raiting for each attributes added.

--- L_Vader.py
+++ L_Vader.py
@@ -1,10 +1,24 @@
 import pandas as pd
+from nltk import word_tokenize
 from termcolor import colored
+from colorama import Fore, Back, Style
+import csv
 import matplotlib.pyplot as plt
+
+class bcolors:
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+
+with open("C:/Users/Demir/Desktop/Final_Project/DataSets/sifatlar1.csv", encoding = "ISO-8859-1", ) as f:
+    reader = csv.reader(f)                  # Read adjactives list from csv
+    data = list(reader)                     # output: list of list, each row is a list which is in list.
+data2 = sum(data, [])                       # list of list convert to list of string.
 
 
 # 1. reviews_df = pd.read_csv("file.cvs", encoding = "ISO-8859-1" )  # Important!
-reviews_df = pd.read_csv("C:/Users/Demir/Desktop/Final_Project/DataSets/London1.csv", encoding = "ISO-8859-1")   # read data   !!!!!
+reviews_df = pd.read_csv("C:/Users/Demir/Desktop/Final_Project/DataSets/London1.csv", encoding = "ISO-8859-1")   # read data !!!!!
 
 print('\t\t\t\t\t--------Hotels--------\n', reviews_df['Property Name'].unique())
 isim = input("Please enter hotel name?")
@@ -35,10 +49,11 @@ def sentiment_scores(sentence):
     #print(sentence, score)
     return score["compound"]
 
+# it doesn't matter whether the words are in sentence, or sentence is contains the words.
 def findSubject(dizi,t):            # This function searchs that does the review has got input word or not.
     colName = str(dizi[0])          # if there is (any), set this column to 1
     for i in dizi:
-        if i in reviews_df_com['Review Text'].values[t]:
+        if i in reviews_df_com['Review Text'].values[t]:        # t : ilgili yorumun indisi sadece bir yoruma bakıyor
             reviews_df_com[colName].values[t] = 1
 
 if __name__ == "__main__":
@@ -72,7 +87,6 @@ if __name__ == "__main__":
     #------------------------------ Vader -----------------------------------------
     for t in range(len(reviews_df_com)):                        # iterate for each object
         i = str(reviews_df_com['Review Text'].values[t])        # Take just reviews to String : i
-        #print("\n", i)
         sonuc = sentiment_scores(i)     # İlgili yorumu i dizisine aldık şimdi yorumu SA yaptırıyoruz sonuçta compound dönüyor.
         #sentiment_scores(clean_text(i))
 
@@ -84,7 +98,7 @@ if __name__ == "__main__":
             reviews_df_com['Tag'].values[t] = 0
         else:                                              # Negative // sonuc < -0.05
             reviews_df_com['Tag'].values[t] = -1
-            print(colored(i, 'red'), sonuc)
+            # print(colored(i, 'red'), sonuc)
 
         findSubject(staff, t)
         findSubject(loc, t)
@@ -108,10 +122,15 @@ if __name__ == "__main__":
         dfneg = len(df1[df1["Tag"] == -1])
         dfnotr = len(df1[df1["Tag"] == 0])
 
-        print(i, " : ", len(reviews_df_com[reviews_df_com[i] == 1]), "/", len(reviews_df_com.index), " rewiew is relevant")
-        print(" + : ", dfpos,"/", len(reviews_df_com[reviews_df_com[i] == 1]))
-        print(" - : ", dfneg,"/", len(reviews_df_com[reviews_df_com[i] == 1]))
-        print(" 0 : ", dfnotr,"/", len(reviews_df_com[reviews_df_com[i] == 1]))
+        sumRating = df1['Review Rating'].sum(axis=0, skipna = True)
+        count = len(df1.index)
+        avgRating = format(sumRating/count, '.4f')
+
+        print(i, " : ", len(reviews_df_com[reviews_df_com[i] == 1]), "/", len(reviews_df_com.index), " rewiew is relevant." , Fore.BLUE + "Avarage Rating:",avgRating )
+        print(Fore.GREEN  +  " + : {} / {} ".format(dfpos,  len(reviews_df_com[reviews_df_com[i] == 1])))
+        print(Fore.YELLOW +  " 0 : {} / {} ".format(dfnotr, len(reviews_df_com[reviews_df_com[i] == 1])))
+        print(Fore.RED    +  " - : {} / {} ".format(dfneg,  len(reviews_df_com[reviews_df_com[i] == 1])))
+        print(Style.RESET_ALL)
         print()
 
         #-------------------------Graphic---------------------------
@@ -122,6 +141,32 @@ if __name__ == "__main__":
         # plt.legend()
         # plt.show()
 
+    print(attributes)
+    look1 = look = input("which reviews you search from above?")
+
+    if look == 'staff': look = staff
+    elif look == 'location': look = loc
+    elif look == 'room': look = room
+    elif look == 'breakfast': look = breakfast
+    elif look == 'bed': look = bed
+    elif look == 'service': look = service
+    elif look == 'bathroom': look = bath
+    elif look == 'view': look = view
+    elif look == 'food': look = food
+    elif look == 'restaurant': look = rest
+    else: print("Wrong choices")
+
+    df2 = df1[df1[look1] == 1]
+    for i in df2['Review Text']:
+        i = word_tokenize(i)
+        for t in i:
+            if t in data2:
+                print(colored(t, 'green'), end=' ')
+            elif t in look:
+                print(colored(t, 'blue'), end=' ')
+            else:
+                print(t, end=' ')
+        print()
 
 
 
